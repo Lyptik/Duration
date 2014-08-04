407ef3462a87922abf7784b4c6589bb8c33abaad
MatteoFantasy <matteo_fantasy@hotmail.fr>
2014-08-05 00:54:16 +0300

    First tests of the serial senser (not working yet)

---
+++ Duration/src/serialSender.cpp
@@ -0,0 +1,9 @@
+//
+//  serialSender.cpp
+//  Duration
+//
+//  Created by Theoriz-MacMini on 8/5/14.
+//
+//
+
+#include "serialSender.h"
---
+++ Duration/src/serialSender.h
@@ -0,0 +1,14 @@
+//
+//  serialSender.h
+//  Duration
+//
+//  Created by Theoriz-MacMini on 8/5/14.
+//
+//
+
+#ifndef Duration_serialSender_h
+#define Duration_serialSender_h
+
+
+
+#endif
